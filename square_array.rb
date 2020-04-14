8e92ea373947a191d21e06786f4ed9506bfa714c
Tanattha Thuenim <tanattha.thuenim@gmail.com>
2020-04-14 16:01:11 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,8 @@
 def square_array(array)
-  squared = []
-  array.each { |element| squared << element ** 2 }
-  squared
+  square = []
+ # array.each { |num| square << num ** 2 }
+ 
+ array.each do |num|
+   square << num ** 2
+  square
 end
